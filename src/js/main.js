cfeb86c096cc8ea025378843f04f72da2471e917
Sarah <sarahwwatkins@gmail.com>
2023-08-22 11:10:52 -0400

    initial commit

---
+++ src/js/main.js
@@ -0,0 +1,35 @@
+import '../scss/styles.scss'
+import * as bootstrap from 'bootstrap'
+import '../scss/blog.css'
+import '../js/color-modes.js'
+
+
+const toastTrigger = document.getElementsByClassName('liveToastBtn')[0]
+const toastLiveExample = document.getElementById('liveToast')
+
+if (toastTrigger) {
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+  toastTrigger.addEventListener('click', () => {
+    toastBootstrap.show()
+  })
+}
+
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const appendAlert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    appendAlert('Nice, you triggered this alert message!', 'success')
+  })
+}
